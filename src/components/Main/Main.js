abac7f003594ee0e7495ef5af4a3af0faf6c2964
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-03-16 02:01:25 +0300

    Feat: Add AboutProject component && styles

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
   return (
     <section className="main">
       <Promo />
       <NavTab />
+      <AboutProject />
     </section>
   );
 }
