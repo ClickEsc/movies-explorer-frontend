384cc577e6539003224a1ee37622513c8d151270
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-03-15 23:01:50 +0300

    Feat: Add NavTab component && styles

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,11 @@
-import Navigation from '../Navigation/Navigation';
 import Promo from '../Promo/Promo';
+import NavTab from '../NavTab/NavTab';
 
 function Main() {
   return (
     <section className="main">
-      <Navigation />
       <Promo />
+      <NavTab />
     </section>
   );
 }
