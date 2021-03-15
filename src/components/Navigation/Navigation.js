2672bb4bba591f26b2e9a49eba8b0017ff6ecf69
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-03-15 22:21:47 +0300

    Feat: Add Navigation component && styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,14 @@
+import logo from "../../images/navigation__logo.svg";
 import './Navigation.css';
 
 function Navigation() {
   return (
     <section className="navigation">
+      <img className="navigation__logo" src={logo} alt="Стилизованный логотип сервиса обзора фильмов 'Movies Explorer'"/>
+      <div className="navigation__container">
+        <button className="navigation__button navigation__button_signup">Регистрация</button>
+        <button className="navigation__button navigation__button_signin">Войти</button>
+      </div>
     </section>
   );
 }
