87f6ece43bff6b5cc0d0d7c5c46e38c8f5b2ba6f
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-03-18 15:36:27 +0300

    Feat: Add MovieCard component && styles

---
+++ src/components/MoviesCard/MoviesCard.js
@@ -0,0 +1,20 @@
+import './MoviesCard.css'
+
+function MoviesCard(props) {
+  return (
+    <div className="movies-card">
+      <img className="movies-card__image" src={props.src} alt={`Кадр из фильма ${props.name}`} />
+      { props.isSaved ? 
+        <button className="movies-card__button movies-card__button_unfav"></button>
+      :
+        props.showButton ? <button className="movies-card__button movies-card__button_fav">Сохранить</button> : ''
+      }
+      <div className="movies-card__info">
+        <h3 className="movies-card__title">{props.name}</h3>
+        <p className="movies-card__duration">1ч 17м</p>
+      </div>
+    </div>
+  )
+};
+
+export default MoviesCard;
