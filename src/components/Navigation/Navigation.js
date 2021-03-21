d76e931dbca2201f3c7ae1c996811d30d0035c3c
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-03-21 14:51:23 +0300

    Refactor Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,23 +1,22 @@
 import { Link } from 'react-router-dom';
 import { pathname } from '../../utils/constants';
-
 import './Navigation.css';
 
 function Navigation() {
   return (
     <section className="navigation">
-      { (pathname === "/movies") &&
-        <div className="navigation__wrap">
-          <Link to="/movies" className="navigation__link navigation__link_active navigation__link_movies">Фильмы</Link>
-          <Link to="/saved-movies" className="navigation__link navigation__link_saved-movies">Сохранённые фильмы</Link>
-        </div> 
-      }
-      { (pathname === "/saved-movies") &&
-        <div className="navigation__wrap">
-          <Link to="/movies" className="navigation__link navigation__link_movies">Фильмы</Link>
-          <Link to="/saved-movies" className="navigation__link navigation__link_active navigation__link_saved-movies">Сохранённые фильмы</Link>
-        </div> 
-      }
+        <div className="navigation__container">
+          <Link
+            to="/movies"
+            className={`navigation__link ${(pathname === "/movies") && 'navigation__link_active'}`}>
+            Фильмы
+          </Link>
+          <Link
+            to="/saved-movies"
+            className={`navigation__link ${(pathname === "/saved-movies") && 'navigation__link_active'}`}>
+            Сохранённые фильмы
+          </Link>
+        </div>
     </section>
   );
 }
