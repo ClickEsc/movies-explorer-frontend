f7308ada2ac5d0be2e2564df15e79b8a3ee1f028
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-03-26 17:19:41 +0300

    Feat: Add loading more films display

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import { pathname } from '../../utils/constants';
 
@@ -5,11 +6,33 @@ import './MoviesCardList.css';
 
 function MoviesCardList(props) {
 
-  const renderedMovies = props.movies.map((item) => (
+  const [moviesCount, setMoviesCount] = React.useState(0);
+
+  const renderedMovies = props.movies.slice(0, moviesCount).map((item) => (
     <li key={item.id} className="movies-cards__card">
       <MoviesCard movie={item} />
     </li>
   ));
+
+  function handleMoviesCount() {
+    if (!props.isMobile) {
+      setMoviesCount(12);
+    } else if (props.isSuperMobile) {
+      setMoviesCount(5);
+    } else if (props.isMobile && !props.isSuperMobile) {
+      setMoviesCount(8);
+    } 
+  }
+
+  function loadMore() {
+    if (!props.isMobile) {
+      setMoviesCount(moviesCount + 3);
+    } else if (props.isSuperMobile) {
+      setMoviesCount(moviesCount + 2);
+    } else if (props.isMobile && !props.isSuperMobile) {
+      setMoviesCount(moviesCount + 2);
+    } 
+  }
 
   function showHint() {
     if (pathname === "/movies") {
@@ -19,11 +42,18 @@ function MoviesCardList(props) {
     }
   }
 
+  React.useEffect(() => {
+    handleMoviesCount();
+  }, []);
+
   return (
-    <ul className="movies-cards">
-      {renderedMovies && renderedMovies}
-      {!renderedMovies && <p className="movies-cards__hint">{showHint()}</p>}
-    </ul>
+    <>
+      <ul className="movies-cards">
+        {renderedMovies && renderedMovies}
+        {!renderedMovies && <p className="movies-cards__hint">{showHint()}</p>}
+      </ul>
+      {pathname === "/movies" && renderedMovies && <button onClick={loadMore} className="movies-cards__load-more" type="button">Ещё</button>}
+    </>
   )
 };
 
