79c69f9aa35e8c67b9b145b188d7acb089508723
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-03-26 11:39:54 +0300

    Feat: Add useLocation

--- src/components/PopupMenu/PopupMenu.js
+++ src/components/PopupMenu/PopupMenu.js
@@ -1,10 +1,24 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import AccountLink from '../AccountLink/AccountLink';
-import { pathname } from '../../utils/constants';
 import cross from '../../images/popup__close-icon.svg';
 import './PopupMenu.css';
 
 function PopupMenu(props) {
+
+  const location = useLocation();
+  const path = location.pathname;
+
+  const [activeLink, setActiveLink] = React.useState('');
+
+  function toggleActiveLink() {
+    setActiveLink('popup__link_active');
+  }
+
+  React.useEffect(() => {
+    toggleActiveLink();
+  }, [location]);
+
   return (
     <div className={`popup ${props.isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__container">
@@ -12,28 +26,28 @@ function PopupMenu(props) {
           <li className="popup__list-item">
             <Link
               to="/"
-              className={`popup__link ${(pathname === "/") ? 'popup__link_active' : ''}`}>
+              className={`popup__link ${path === "/" ? activeLink : ''}`}>
                 Главная
             </Link>
           </li>
           <li className="popup__list-item">
             <Link
               to="/movies"
-              className={`popup__link ${(pathname === "/movies") ? 'popup__link_active' : ''}`}>
+              className={`popup__link ${path === "/movies" ? activeLink : ''}`}>
                 Фильмы
             </Link>
           </li>
           <li className="popup__list-item">
             <Link
               to="/saved-movies"
-              className={`popup__link ${(pathname === "/saved-movies") ? 'popup__link_active' : ''}`}>
+              className={`popup__link ${path === "/saved-movies" ? activeLink : ''}`}>
                 Сохранённые фильмы
             </Link>
           </li>
         </ul>
         <AccountLink />
         <button className="popup__close-button" type="button">
-          <img classname="popup__close-icon" src={cross} alt="Крестик" />
+          <img onClick={props.onClose} classname="popup__close-icon" src={cross} alt="Крестик" />
         </button>
       </div>
     </div>
